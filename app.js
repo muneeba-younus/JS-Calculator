2abdbb2ba2fde5c40323b96962174d2175e6a9b1
Muneeba Younus <50373423+muneeba-younus@users.noreply.github.com>
2020-06-26 02:20:53 +0500

    Commit Completed

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 function getNumber(num) {
-    var equation = document.getElementById("equation");
-    equation.value += num;
+    var answer = document.getElementById("answer");
+    answer.value += num;
 }
 function clearBtn() {
-    var equation = document.getElementById("equation");
-    equation.value = "";
+    var answer = document.getElementById("answer");
+    answer.value = "";
 }
 function getSolution() {
     var answer = document.getElementById("answer");
-    answer.value = eval(equation.value)
+
+    answer.value = eval(answer.value)
 }
 function getPercentage() {
-    var equation = document.getElementById("equation");
-    equation.value = eval(equation.value)
-    answer.value = (equation.value / 100)
+    var answer = document.getElementById("answer");
+    answer.value = eval(answer.value)
+    answer.value = (answer.value / 100)
+}
+function getSquare() {
+    var answer = document.getElementById("answer");
+    answer.value = answer.value * answer.value;
+}
+function getCube() {
+    var answer = document.getElementById("answer");
+    answer.value = answer.value * answer.value;
+}
+function getSquareRoot() {
+    var answer = document.getElementById("answer");
+    answer.value = Math.sqrt(answer.value);
+}
+function getSine() {
+    var answer = document.getElementById("answer");
+    answer.value = Math.sin(answer.value);
+}
+function getCosine() {
+    var answer = document.getElementById("answer");
+    answer.value = Math.cos(answer.value);
+}
+function getTan() {
+    var answer = document.getElementById("answer");
+    answer.value = Math.tan(answer.value);
 }
